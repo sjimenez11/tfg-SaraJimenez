68142f67b41837baa3b4d0c3a9150dce3d60b279
Sara Jimenez <sjimenez11@educa.madrid.org>
2024-06-04 20:54:40 +0200

    Initial commit

---
+++ Desktop/tfg/connectedReads-backend/src/main/java/com/connectedReads/ConnectedReadsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.connectedReads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectedReadsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectedReadsBackendApplication.class, args);
+	}
+
+}
